582b124db6629595958882b12815cafe74a0c994
Besoninja <152490543+Besoninja@users.noreply.github.com>
2024-12-06 22:13:22 +1100

    Update Adv_PB_Sim.py

--- Adv_PB_Sim.py
+++ Adv_PB_Sim.py
@@ -144,7 +144,7 @@ if st.button('Play Games'):
         table_data = [
             {
                 "Winning Combination": times_won_labels[key],
-                "Potential Earnings": prize_values.value(),
+                "Potential Earnings": f"${prize_values.[key]:,.2f}"
                 "Count": value,
                 "Simulated Payout": f"${value * prize_values[key]:,.2f}"
             }
